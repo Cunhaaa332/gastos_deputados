a8bd7bc41169c1be2a44a1d4111b14c07eccb0db
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-17 20:30:28 -0300

    adding two main methods in deputados_controller

--- app/controllers/deputados_controller.rb
+++ app/controllers/deputados_controller.rb
@@ -1,2 +1,40 @@
 class DeputadosController < ApplicationController
+  def index
+    uf = params[:uf].upcase
+    deputados = Deputado.includes(:despesas).where(uf: uf)
+
+    render json: deputados.map{|dep|
+      {
+        id: dep.id,
+        nome: dep.nome,
+        partido: dep.partido,
+        uf: dep.uf,
+        total_de_gastos: dep.despesas.sum(:vlrLiquido)
+      }
+    }
+  end
+
+  def despesas
+    deputado = Deputado.includes(:despesas).find(params[:id])
+
+    maior_despesa = deputado.despesas.order(vlrLiquido: :desc).first
+
+    render json: {
+      deputado: {
+        id: deputado.id,
+        nome: deputado.nome,
+        partido: deputado.partido,
+        uf: deputado.uf
+      },
+      despesas: deputado.despesas.map {|desp|
+        {
+          fornecedor: desp.txtFornecedor,
+          data_emissao: desp.datEmissao,
+          valor: desp.vlrLiquido,
+          url_documento: desp.urlDocumento,
+          maior_despesa: desp == maior_despesa
+        }
+      }
+    }
+  end
 end
