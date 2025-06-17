d521eab7d2ac9afbff5e5965513d5dd9ca2fcb8a
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-17 17:02:38 -0300

    add date_parse method

--- app/services/csv_importer.rb
+++ app/services/csv_importer.rb
@@ -16,12 +16,26 @@ class CsvImporter
       unless deputado.despesas.exists?(txtFornecedor: row['txtFornecedor'], datEmissao: row['datEmissao'], vlrLiquido: row['vlrLiquido'])
         deputado.despesas.create!(
           txtFornecedor: row['txtFornecedor'],
-          datEmissao: Date.parse(row['datEmissao']) rescue nil,
+          datEmissao: date_parse(row['datEmissao']),
           vlrLiquido: row['vlrLiquido'].to_f,
           urlDocumento: row['urlDocumento']
         )
       end
       puts("Deputado: #{deputado.nome} importado com despesa de: #{row['vlrLiquido'].to_f}")
+    end
+  end
+
+  private
+
+  def self.date_parse(str_date)
+    return nil if str_date.nil?
+    begin
+      Date.strptime(str_date, '%Y-%m-%d')
+      begin
+        Date.strptime(str_date, '%d/%m/%Y')
+      rescue ArgumentError
+        nil
+      end
     end
   end
 end
