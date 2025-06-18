76f1b5087063928b83ffdcae60e29e785c99d8b7
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-17 21:36:08 -0300

    add initial style to index

--- app/controllers/deputados_controller.rb
+++ app/controllers/deputados_controller.rb
@@ -1,38 +1,44 @@
 class DeputadosController < ApplicationController
   def index
     uf = params[:uf].upcase
-    deputados = Deputado.includes(:despesas).where(uf: uf)
-
-    render json: deputados.map{|dep|
-      {
-        id: dep.id,
-        nome: dep.nome,
-        partido: dep.partido,
-        uf: dep.uf,
-        total_de_gastos: dep.despesas.sum(:vlrLiquido)
-      }
-    }
+    @deputados = Deputado.includes(:despesas)
+                          .where(uf: uf)
+                          .sort_by { |d| -d.despesas.sum(:vlrLiquido) }
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @deputados.map{|dep|
+          {
+            id: dep.id,
+            nome: dep.nome,
+            partido: dep.partido,
+            uf: dep.uf,
+            total_de_gastos: dep.despesas.sum(:vlrLiquido)
+          }
+        }
+      end
+    end
   end
 
   def despesas
-    deputado = Deputado.includes(:despesas).find(params[:id])
+    @deputado = Deputado.includes(:despesas).find(params[:id])
 
-    maior_despesa = deputado.despesas.order(vlrLiquido: :desc).first
+    @maior_despesa = @deputado.despesas.order(vlrLiquido: :desc).first
 
     render json: {
       deputado: {
-        id: deputado.id,
-        nome: deputado.nome,
-        partido: deputado.partido,
-        uf: deputado.uf
+        id: @deputado.id,
+        nome: @deputado.nome,
+        partido: @deputado.partido,
+        uf: @deputado.uf
       },
-      despesas: deputado.despesas.map {|desp|
+      despesas: @deputado.despesas.map {|desp|
         {
           fornecedor: desp.txtFornecedor,
           data_emissao: desp.datEmissao,
           valor: desp.vlrLiquido,
           url_documento: desp.urlDocumento,
-          maior_despesa: desp == maior_despesa
+          maior_despesa: desp == @maior_despesa
         }
       }
     }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'deputados/:uf', to: 'deputados#index'
-  get 'deputado/:id/despesas', to: 'deputados#despesas'
+  get 'deputado/:id/despesas', to: 'deputados#despesas', as: :deputado_despesas
 end
