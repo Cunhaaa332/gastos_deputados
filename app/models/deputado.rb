7ddd3c1ae2c7affb747d3e48d6b1d9b520bba3e2
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-18 00:59:11 -0300

    add gasto_total_por_partido method

--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -1,6 +1,15 @@
 class Deputado < ApplicationRecord
   has_many :despesas, dependent: :destroy
 
+  def self.gasto_total_por_partido(uf)
+    where(uf: uf)
+    .includes(:despesas)
+    .group_by(&:partido)
+    .transform_values { |deps| deps.sum { |d| d.despesas.sum(:vlrLiquido).to_f.round(2) } }
+    .sort_by { |_, total| -total }
+    .to_h
+  end
+
   def maior_despesa
     despesas.order(vlrLiquido: :desc).first
   end
