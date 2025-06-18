a3ae6cebe7fdac326e75b0e976dfbcb7428f808b
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-17 22:02:23 -0300

    refactoring deputados_controller

--- app/controllers/deputados_controller.rb
+++ app/controllers/deputados_controller.rb
@@ -6,17 +6,7 @@ class DeputadosController < ApplicationController
                           .sort_by { |d| -d.despesas.sum(:vlrLiquido) }
     respond_to do |format|
       format.html
-      format.json do
-        render json: @deputados.map{|dep|
-          {
-            id: dep.id,
-            nome: dep.nome,
-            partido: dep.partido,
-            uf: dep.uf,
-            total_de_gastos: dep.despesas.sum(:vlrLiquido)
-          }
-        }
-      end
+      format.json {render json: deputados_json }
     end
   end
 
@@ -25,22 +15,44 @@ class DeputadosController < ApplicationController
 
     @maior_despesa = @deputado.maior_despesa
 
-    render json: {
-      deputado: {
-        id: @deputado.id,
-        nome: @deputado.nome,
-        partido: @deputado.partido,
-        uf: @deputado.uf
-      },
-      despesas: @deputado.despesas.map {|desp|
-        {
-          fornecedor: desp.txtFornecedor,
-          data_emissao: desp.datEmissao,
-          valor: desp.vlrLiquido,
-          url_documento: desp.urlDocumento,
-          maior_despesa: desp == @maior_despesa
-        }
-      }
+    respond_to do |format|
+      format.html
+      format.json {render json: despesas_json}
+    end
+  end
+end
+
+
+private
+
+def deputados_json
+  @deputados.map do |dep|
+    {
+      id: dep.id,
+      nome: dep.nome,
+      partido: dep.partido,
+      uf: dep.uf,
+      total_de_gastos: dep.despesas.sum(:vlrLiquido)
     }
   end
+end
+
+def despesas_json
+  {
+    deputado: {
+      id: @deputado.id,
+      nome: @deputado.nome,
+      partido: @deputado.partido,
+      uf: @deputado.uf
+    },
+    despesas: @deputado.despesas.map {|desp|
+      {
+        fornecedor: desp.txtFornecedor,
+        data_emissao: desp.datEmissao,
+        valor: desp.vlrLiquido,
+        url_documento: desp.urlDocumento,
+        maior_despesa: desp == @maior_despesa
+      }
+    }
+  }
 end
