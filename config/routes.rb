bd2368168ed6e6c3de16c614b7b4c52aff79c1c1
Cunhaaa332 <gabriel.dsilveira@al.infnet.edu.br>
2025-06-18 13:48:42 -0300

    add post route to import csv

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'deputados(/:uf)', to: 'deputados#index', as: :deputados
   get 'deputado/:id/despesas', to: 'deputados#despesas', as: :deputado_despesas
+
+  post "import_csv", to: "deputados#import_csv", as: :import_csv
 end
